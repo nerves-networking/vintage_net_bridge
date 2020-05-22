cb2c4bc75312804cefb707b7b7e8795eea3f4f4e
Frank Hunleth <fhunleth@troodon-software.com>
2020-05-22 14:28:21 -0400

    Project doc/infra updates for publishing

--- README.md
+++ README.md
@@ -1,12 +1,21 @@
-# VintageNetBridge
+![VintageNet logo](assets/logo.png)
 
-`VintageNetBridge` makes it easy to add and remove interfaces from a
-linux bridge. 
+[![Hex version](https://img.shields.io/hexpm/v/vintage_net_bridge.svg "Hex version")](https://hex.pm/packages/vintage_net_bridge)
+[![API docs](https://img.shields.io/hexpm/v/vintage_net_bridge.svg?label=hexdocs "API docs")](https://hexdocs.pm/vintage_net_bridge/VintageNetEthernet.html)
+[![CircleCI](https://circleci.com/gh/nerves-networking/vintage_net_bridge.svg?style=svg)](https://circleci.com/gh/nerves-networking/vintage_net_bridge)
+[![Coverage Status](https://coveralls.io/repos/github/nerves-networking/vintage_net_bridge/badge.svg?branch=master)](https://coveralls.io/github/nerves-networking/vintage_net_bridge?branch=master)
 
-## Installation
+`VintageNetBridge` adds support for creating Ethernet bridges with `VintageNet`.
+Bridging two networks joins them together so that they form one LAN. One use
+case would be to join a WiFi mesh network with a wired Ethernet LAN. Devices on
+the mesh network would look like they're on the Ethernet LAN when in reality all
+traffic was being transferred through the bridge. For example, a DHCP server
+running on the Ethernet LAN would provide IP addresses for the WiFi mesh
+devices. While bridging is not always an appropriate way of joining networks, in
+this case, it enables one to provision multiple Mesh WiFi to Ethernet LAN
+devices for redundancy in the mesh while still looking like a normal IPv4 LAN.
 
-If [available in Hex](https://hex.pm/docs/publish), the package can be installed
-by adding `vintage_net_bridge` to your list of dependencies in `mix.exs`:
+To use, add `:vintage_net_bridge` to your `mix` dependencies like this:
 
 ```elixir
 def deps do
@@ -16,7 +25,10 @@ def deps do
 end
 ```
 
-add the following to your config.exs:
+## Using
+
+Bridges typically have names like `"br0"`, `"br1"`, etc. Here is an example
+configuration:
 
 ```elixir
 config :vintage_net,
@@ -25,13 +37,11 @@ config :vintage_net,
     {"eth0",
      %{
        type: VintageNetEthernet,
-       # ipv4: %{method: :dhcp}
        ipv4: %{method: :disabled}
      }},
     {"br0",
      %{
        type: VintageNetBridge,
-       # ipv4: %{method: :disabled},
        ipv4: %{method: :dhcp},
        vintage_net_bridge: %{
          interfaces: ["eth0", "mesh0"]
@@ -40,7 +50,6 @@ config :vintage_net,
     {"mesh0",
      %{
        type: VintageNetWiFi,
-       # ipv4: %{method: :dhcp},
        ipv4: %{method: :disabled},
        vintage_net_wifi: %{
          user_mpm: 1,
--- mix.exs
+++ mix.exs
@@ -2,14 +2,13 @@ defmodule VintageNetBridge.MixProject do
   use Mix.Project
 
   @version "0.7.0"
-  @source_url "https://github.com/nerves-networking/vintage_net_wifi"
+  @source_url "https://github.com/nerves-networking/vintage_net_bridge"
 
   def project do
     [
       app: :vintage_net_bridge,
       version: @version,
       elixir: "~> 1.7",
-      elixirc_paths: elixirc_paths(Mix.env()),
       test_coverage: [tool: ExCoveralls],
       start_permanent: Mix.env() == :prod,
       build_embedded: true,
@@ -21,61 +20,34 @@ defmodule VintageNetBridge.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
-      config: [],
-      max_interface_count: 8,
-      tmpdir: "/tmp/vintage_net",
-      to_elixir_socket: "comms",
-      bin_dnsd: "dnsd",
-      bin_ifup: "ifup",
-      bin_ifdown: "ifdown",
-      bin_ifconfig: "ifconfig",
-      bin_chat: "chat",
-      bin_pppd: "pppd",
-      bin_mknod: "mknod",
-      bin_killall: "killall",
-      bin_wpa_supplicant: "/usr/sbin/wpa_supplicant",
-      bin_ip: "ip",
-      bin_udhcpc: "udhcpc",
-      bin_udhcpd: "udhcpd",
-      bin_brctl: "brctl",
-      path: "/usr/sbin:/usr/bin:/sbin:/bin",
-      udhcpc_handler: VintageNet.Interface.Udhcpc,
-      udhcpd_handler: VintageNet.Interface.Udhcpd,
-      resolvconf: "/etc/resolv.conf",
-      persistence: VintageNet.Persistence.FlatFile,
-      persistence_dir: "/root/vintage_net",
-      persistence_secret: "obfuscate_things",
-      internet_host: {1, 1, 1, 1},
-      regulatory_domain: "00",
-      # Contain processes in cgroups by setting to:
-      #   [cgroup_base: "vintage_net", cgroup_controllers: ["cpu"]]
-      muontrap_options: []
+      extra_applications: [:logger]
     ]
   end
 
-  defp elixirc_paths(:test), do: ["lib", "test/support"]
-  defp elixirc_paths(_), do: ["lib"]
-
   defp description do
-    "bridge networking for VintageNet"
+    "Bridge networking for VintageNet"
   end
 
   defp package do
     %{
+      files: [
+        "lib",
+        "test",
+        "mix.exs",
+        "README.md",
+        "LICENSE",
+        "CHANGELOG.md"
+      ],
       licenses: ["Apache-2.0"],
       links: %{"GitHub" => @source_url}
     }
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:vintage_net, "~> 0.8.0",
-       github: "nerves-networking/vintage_net", branch: "wait-for-list"},
+      {:vintage_net, "~> 0.8.0-dev", github: "nerves-networking/vintage_net", branch: "master"},
       {:credo, "~> 1.2", only: :test, runtime: false},
       {:dialyxir, "~> 1.0.0", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.19", only: :docs, runtime: false},

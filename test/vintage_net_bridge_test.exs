5e8c91e49c9930249d07bf5d78782fcb0ecaf750
Frank Hunleth <fhunleth@troodon-software.com>
2020-05-26 15:10:52 -0400

    Comment out unimplemented tests for CI

--- test/vintage_net_bridge_test.exs
+++ test/vintage_net_bridge_test.exs
@@ -2,6 +2,6 @@ defmodule VintageNetBridgeTest do
   use ExUnit.Case
   doctest VintageNetBridge
 
-  test "basic bridge"
-  test "teardown works"
+  # test "basic bridge"
+  # test "teardown works"
 end

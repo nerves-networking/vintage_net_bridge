27e225218cc300ef5057d178ddfbb6cf573b1639
Frank Hunleth <fhunleth@troodon-software.com>
2020-05-27 14:28:36 -0400

    Remove todos

--- lib/vintage_net_bridge.ex
+++ lib/vintage_net_bridge.ex
@@ -80,15 +80,14 @@ defmodule VintageNetBridge do
   end
 
   @impl true
-  # TODO(connor) add support for modifying the bridge
-  # in real time
   def ioctl(_ifname, _command, _args) do
     {:error, :unsupported}
   end
 
   @impl true
-  # TODO(connor) check for brctl utility
-  def check_system(_opts), do: :ok
+  def check_system(_opts) do
+    {:error, "unimplemented"}
+  end
 
   defp config_to_cmd({:forward_delay, value}, brctl, ifname) do
     [{:run, brctl, ["setfd", ifname, to_string(value)]}]

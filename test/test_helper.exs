f79a34953253fd7767b2826daa02a1a96d992af5
Connor Rigby <connorr@hey.com>
2021-04-09 13:39:36 -0700

    Update to latest VintageNet

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -3,30 +3,61 @@ defmodule Utils do
 
   @spec default_opts() :: keyword()
   def default_opts() do
-    # Use the defaults in mix.exs, but normalize the paths to commands
     Application.get_all_env(:vintage_net)
   end
 
   @spec udhcpc_child_spec(VintageNet.ifname(), String.t()) :: Supervisor.child_spec()
   def udhcpc_child_spec(ifname, hostname) do
+    beam_notify = Application.app_dir(:beam_notify, "priv/beam_notify")
+    env = BEAMNotify.env(name: "vintage_net_comm", report_env: true)
+
     %{
       id: :udhcpc,
+      start: {
+        VintageNet.Interface.IfupDaemon,
+        :start_link,
+        [
+          [
+            ifname: ifname,
+            command: "udhcpc",
+            args: [
+              "-f",
+              "-i",
+              ifname,
+              "-x",
+              "hostname:#{hostname}",
+              "-s",
+              beam_notify
+            ],
+            opts: [
+              stderr_to_stdout: true,
+              log_output: :debug,
+              log_prefix: "udhcpc(#{ifname}): ",
+              env: env
+            ]
+          ]
+        ]
+      }
+    }
+  end
+
+  @spec udhcpd_child_spec(VintageNet.ifname()) :: Supervisor.child_spec()
+  def udhcpd_child_spec(ifname) do
+    env = BEAMNotify.env(name: "vintage_net_comm", report_env: true)
+
+    %{
+      id: :udhcpd,
       restart: :permanent,
       shutdown: 500,
       start:
         {MuonTrap.Daemon, :start_link,
          [
-           "udhcpc",
+           "udhcpd",
            [
              "-f",
-             "-i",
-             ifname,
-             "-x",
-             "hostname:#{hostname}",
-             "-s",
-             Application.app_dir(:vintage_net, ["priv", "udhcpc_handler"])
+             "/tmp/vintage_net/udhcpd.conf.#{ifname}"
            ],
-           [stderr_to_stdout: true, log_output: :debug, log_prefix: "udhcpc(#{ifname}): "]
+           [stderr_to_stdout: true, log_output: :debug, env: env]
          ]},
       type: :worker
     }
